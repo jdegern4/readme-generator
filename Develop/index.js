36b3e3f140d9331ef06738941461d7aab6f996ee
Jordan D <degco44@gmail.com>
2022-07-03 20:26:23 -0500

    writeToFile and init functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,13 @@
 const inquirer = require("inquirer");
 const {writeFile, copyFile} = require("")
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Include packages needed for this application
-const generatePage = require()
-// TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'title',
-            message: 'What is the name of your project? (Required)',
+            message: 'What is the name of your project?',
             validate: nameInput => {
                 if (nameInput) {
                     return true;
@@ -22,7 +20,7 @@ const questions = () => {
         {
             type: 'input',
             name: 'description',
-            message: 'Please provide a brief description of the functionality of your project (Required)',
+            message: 'Please provide a brief description of the functionality of your project.',
             validate: userInput => {
                 if (userInput) {
                     return true;
@@ -36,30 +34,75 @@ const questions = () => {
             type: 'input',
             name: 'installation',
             message: 'What does the user need to do to install your application?',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('Please enter installation instructions!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'usage',
             message: 'What usage information does the user need to know?',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('Please enter usage information!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'contribution',
             message: 'What are the guidelines for people who wish to contribute to the project?',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a list of contributors!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'tests',
-            message: 'What are the instructions for testing your application?',  
+            message: 'What are the instructions for testing your application? (CLI commands, etc)',
         },
     ])
-}
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(answer) {
+        console.log(answer);
+        let readmeContent = generateMarkdown(answer);
+        writeFile(readmeContent);
+    })
+};
 
 // Function call to initialize app
 init();
+
+module.exports = questions;
